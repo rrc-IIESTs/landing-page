fc23861284eaf6cdd6540910c4a724f4ced4ab24
Reetu Raj Chauhan <rtrjcn@gmail.com>
2022-12-30 23:58:06 +0530

    Upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function submitForm() {
+    let fname = document.getElementById('first_name').value;
+    let lname = document.getElementById('last_name').value;
+    let email = document.getElementById('email').value;
+    let ReasonForContacting = document.getElementById('reason').value;
+    if ( fname!=null && lname!=null  && email!=null && ReasonForContacting!=null ) {
+         alert("Form submitted successfully.")
+    }
+     else {
+        alert("All required fields must be filled")
+    }
+}
